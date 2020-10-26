cca5354d90c93372bfce44f663ab4446c8aa03f5
Lourine <lourine.millicent@gmail.com>
2020-10-26 03:37:32 +0300

    Add registration paths in projects urls.py

--- awwards/urls.py
+++ awwards/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^myawwards/',include('myawwards.urls'))
+    url(r'',include('myawwards.urls')),
+    url(r'accounts/',include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^logout/',views.LogoutView.as_view,{'next_page':'/'}),
 ]
