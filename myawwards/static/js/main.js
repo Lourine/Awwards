02fcecec274aa16a56c1f5e7f979ae1ee007d04a
Lourine <lourine.millicent@gmail.com>
2020-10-27 08:26:18 +0300

    Add static files

---
+++ myawwards/static/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#love").click(function(){
+    $("#love").hide()
+    $(".form").slideDown(2000)
+      $(".form").show()
+
+  })
+
+  $("#click").click(function(){
+    $("#search").show()
+    $("#click").hide()
+  })
+
+
+
+})
